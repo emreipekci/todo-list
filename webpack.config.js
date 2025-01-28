501b823f014a276bfd30c233d38914468dea854f
emreipekci <153276209+emreipekci@users.noreply.github.com>
2025-01-28 19:40:00 +0100

    added basic UI

--- webpack.config.js
+++ webpack.config.js
@@ -22,10 +22,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
         test: /\.html$/i,
         loader: "html-loader",
       },
@@ -33,6 +29,10 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      }
     ],
   },
 };
